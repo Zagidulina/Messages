b11e2a3353f5afa433748a58621e18714c81f95e
Tata <sandaystorm66@gmail.com>
2022-08-15 23:01:15 +0500

    Initial commit

---
+++ src/Messages.java
@@ -0,0 +1,40 @@
+public class Messages {
+    public static void main(String[] args) {
+
+        String hello = "Привет";
+        String howAreYou = "Как дела?";
+        String good = "Хорошо";
+        String walk = "Пошли гулять?";
+        String where = "Куда пойдем гулять?";
+        String garden = "Сад";
+        String letsGgo = "Идем";
+        String time = "Время";
+        String comfortable = "Удобно";
+        String evening = "Вечера";
+
+
+
+
+        System.out.println("Маша пишет: "+ hello);
+
+        System.out.println("Киррил отвечает: " + hello);
+
+        System.out.println("Маша спрашивает: " + howAreYou);
+
+        System.out.println( "Кирилл отвечает: " + good);
+
+        System.out.println("Кирилл пишет: " + walk);
+
+        System.out.println("Кирилл отвечает:" + "пойдем" + " " + "в" + " " + garden);
+
+        System.out.println("Маша отвечает :" + good + " " + letsGgo);
+
+        System.out.println("Кирилл пишет :" + "в" + " " + "какое" + " " + time + " " + "тебе" + " " + "удобно?");
+
+        System.out.println("Маша отвечает: " + "мне" + " " + comfortable + " " + "в" + " " + "5" + " " + " " + "вечера");
+
+        System.out.println("Кирилл отвечает: " + good);
+
+        System.out.println("Маша пишет: " + "тогда" + " " + "до" + " " + evening) ;
+    }
+}
